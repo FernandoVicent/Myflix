63ceb5ce633048eda6924483d9e35509cfa9120e
FernandoVicent <fernandov1092@gmail.com>
2022-09-14 14:36:59 -0300

    adicionando projeto

---
+++ js/action.js
@@ -0,0 +1,44 @@
+var listaFilmes = 
+[
+"https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg" ,
+"https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+"https://image.tmdb.org/t/p/w185/rqeYMLryjcawh2JeRpCVUDXYM5b.jpg",
+"https://image.tmdb.org/t/p/w185/9E76j2DcQv8LdbX1Wa9jpbDBfw1.jpg",
+"https://image.tmdb.org/t/p/w185/wWwTjKER5a8LRUGHrw86VU0gUy1.jpg",
+"https://image.tmdb.org/t/p/w185/8niel2KjxfcOGmLWQc6p2fo5pUm.jpg",
+
+]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+	document.write("<img src=" + listaFilmes[i] + ">")
+}
+
+var segundaLista = 
+[
+"https://image.tmdb.org/t/p/w185/5qoBMPkhmbDeTH8SEkY3V2flewD.jpg" ,
+"https://image.tmdb.org/t/p/w185/uQWgSRXeYRWCvGIX9LDNBW6XBYD.jpg",
+"https://image.tmdb.org/t/p/w185/mpdP8ak9Ci2xRxxq2Mb6yB4hDFs.jpg",
+"https://image.tmdb.org/t/p/w185/6B3jyUsM7AxwSTXyr1sc6oZRIvC.jpg",
+"https://image.tmdb.org/t/p/w185/xBll0gR0SVZFCeg5lgm5aL7uTVJ.jpg",
+"https://image.tmdb.org/t/p/w185/qnw9610ojLT0jU3lMSZOAFttt1e.jpg"
+]
+
+for (var i = 0; i <segundaLista.length; i++) {
+	document.write("<img src=" + segundaLista[i] + ">")
+
+}
+
+var terceiraLista = 
+[
+"https://image.tmdb.org/t/p/w185/gUaNDUTQ3sVlcRMBpJDEdu4aDsj.jpg" ,
+"https://image.tmdb.org/t/p/w185/ir3cxdjrJJ1r127hCeGneBYe3fS.jpg",
+"https://image.tmdb.org/t/p/w185/rMoVOCw6DF3zC6hS76ZhQMWiXNX.jpg",
+"https://image.tmdb.org/t/p/w185/gQkzmZmZXIvvprfvvPE2EUlk121.jpg",
+"https://image.tmdb.org/t/p/w185/kz7xJLowO4x0BpcB1IJb9uIXgrq.jpg",
+"https://image.tmdb.org/t/p/w185/vARBnfGEcsb83gdtK0JHb8QaLio.jpg"
+]
+
+for (var i = 0; i <terceiraLista.length; i++) {
+	document.write("<img src=" + terceiraLista[i] + ">")
+
+}
