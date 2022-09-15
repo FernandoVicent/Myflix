d3663def44209e5eb4e35cfe21af1d1569ef4d3d
FernandoVicent <fernandov1092@gmail.com>
2022-09-15 17:51:39 -0300

    subindo app Django

---
+++ static/js/action.js
@@ -0,0 +1,33 @@
+$('.apresentacao-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: true,
+  autoplaySpeed: 3000,
+});
+
+$('.carousel-asistidos').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 8,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
