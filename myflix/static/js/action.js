64f46de837fcf9e501436aed0d35ffe06079b69f
FernandoVicent <fernandov1092@gmail.com>
2022-09-16 17:51:39 -0300

    subido estaticos e partials

--- myflix/static/js/action.js
+++ myflix/static/js/action.js
@@ -31,3 +31,28 @@ $('.carousel-asistidos').slick({
     }
   ]
 });
+$('.terror').slick({
+  centerMode: true,
+  centerPadding: '40px',
+  slidesToShow: 8,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
