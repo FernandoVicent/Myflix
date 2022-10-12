a43563c7c6fb3d6b2ab1057302b0adf9d654ef81
FernandoVicent <Fernandov1092@gmail.com>
2022-10-12 01:43:06 -0300

    adicionando AWS S3

--- myflix/settings.py
+++ myflix/settings.py
@@ -144,7 +144,7 @@ AWS_SECRET_ACCESS_KEY = '976BbuUXvKb20ksOCKbfpV3d9n1i2MdyBN9pJqV8'
 AWS_STORAGE_BUCKET_NAME = 'projeto-mflix'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = "public-read"
 
 AWS_LOCATION = 'static'
 
